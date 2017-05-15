915a07685c8057d4e6a03442d690dd93cfd197ca
xulongfei <xulongfei1@jd.com>
2017-05-15 16:34:45 +0800

    更新

--- src/views/public/module/common/common.js
+++ src/views/public/module/common/common.js
@@ -5,68 +5,12 @@ export default {
         prefix = prefix || "./";
 
         let navList = [
-        //     {
-        //     name: "经营分析",
-        //     icon: "fa fa-area-chart",
-        //     subNav: [{
-        //         name: "销售分析",
-        //         thirdNav: [{
-        //             name: "销售分析",
-        //             href: prefix + "xiaoshoufenxi/index.html"
-        //         }, {
-        //             name: "库存动态",
-        //             href: prefix + "kucunfenxi/index.html"
-        //         }, {
-        //             name: "单品效果",
-        //             href: prefix + "danpinfenxi/index.html"
-        //         }]
-        //     }, {
-        //         name: "库存动态",
-        //         href: prefix + "kucunfenxi/index.html"
-        //     }, {
-        //         name: "单品效果",
-        //         href: prefix + "danpinfenxi/index.html"
-        //     }]
-        // }, {
-        //     name: "市场行情",
-        //     icon: "fa fa-line-chart",
-        //     href: prefix + "xiaoshoufenxi/index.html"
-        // },
-        //     {
-        //     name: "营销分析",
-        //     icon: "fa fa-industry",
-        //     subNav: [{
-        //         name: "广告分析",
-        //         thirdNav: [{
-        //             name: "总体分析",
-        //             href: "#zongtifenxi"
-        //         }, {
-        //             name: "直投分析",
-        //             href: "#zhitoufenxi"
-        //         }, {
-        //             name: "快车分析",
-        //             href: "#kuaichefenxi"
-        //         }]
-        //     }, {
-        //         name: "促销分析",
-        //         thirdNav: [{
-        //             name: "效果分析",
-        //             href: "#xiaoguofenxi"
-        //         }]
-        //     }, {
-        //         name: "优惠券分析",
-        //         thirdNav: [{
-        //             name: "效果分析",
-        //             href: "#xiaoguofenxi"
-        //         }]
-        //     }]
-        // },
             {
-            name: "总体概览天",
+            name: "页面1",
             icon: "ivu-icon ivu-icon-calendar",
             href: prefix + "overviewday.html"
         } ,{
-            name: "总体概览小时",
+            name: "页面2",
             icon: "ivu-icon ivu-icon-ios-pulse",
             href: prefix + "overviewtime.html"
         }];
@@ -78,13 +22,13 @@ export default {
 
     footer: () => {
         return Q.Promise((resolve, reject) => {
-            resolve("Copyright © 2004-2017 京东JD.com 版权所有");
+            resolve(`Copyright © 2004-${new Date().getFullYear()} woshilaoge 版权所有`);
         });
     },
 
     sysInfo: () => {
         let _data = {
-            systemName: "实时应用",
+            systemName: "我的应用",
             logoIcon: "ivu-icon ivu-icon-social-windows" //logo样式
         };
         return Q.Promise((resolve, reject) => {
